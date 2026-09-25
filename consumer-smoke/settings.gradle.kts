f5844ad4652689e7a3bc1de85d90621726bb2f26
jason <changhexuefei2011@163.com>
2026-09-25 08:48:18 +0800

    test: add standalone Windows consumer smoke app

---
+++ consumer-smoke/settings.gradle.kts
@@ -0,0 +1,40 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("jvm") version "2.4.20"
+        kotlin("plugin.compose") version "2.4.20"
+        id("org.jetbrains.compose") version "1.12.1"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        fun localSmokeRepo(envName: String) {
+            val repoPath = System.getenv(envName)
+                ?.takeIf { it.isNotBlank() }
+                ?: error("Missing required environment variable: $envName")
+            maven {
+                name = envName
+                url = uri(file(repoPath))
+            }
+        }
+
+        localSmokeRepo("LETS_PLOT_CORE_REPO")
+        localSmokeRepo("LETS_PLOT_KOTLIN_REPO")
+        localSmokeRepo("LETS_PLOT_COMPOSE_REPO")
+
+        mavenCentral()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "lets-plot-compose-consumer-smoke"
