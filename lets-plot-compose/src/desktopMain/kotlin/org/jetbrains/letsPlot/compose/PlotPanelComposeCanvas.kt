17e977f50c7ea26b642129c2bb7f7143b7608a2c
jason <changhexuefei2011@163.com>
2026-09-26 17:30:04 +0800

    refactor: route PlotPanel through desktop renderer seam

--- lets-plot-compose/src/desktopMain/kotlin/org/jetbrains/letsPlot/compose/PlotPanelComposeCanvas.kt
+++ lets-plot-compose/src/desktopMain/kotlin/org/jetbrains/letsPlot/compose/PlotPanelComposeCanvas.kt
@@ -231,8 +231,10 @@ fun PlotPanelComposeCanvas(
                     // this Canvas block whenever it changes.
                     redrawTrigger
 
-                    paintOnSkiaCanvas(
+                    paintPlotOnDesktopCanvas(
                         canvas = drawContext.canvas.nativeCanvas,
+                        width = size.width.toInt(),
+                        height = size.height.toInt(),
                         density = density.toDouble(),
                         plotPosition = plotPosition
                     ) { context ->
